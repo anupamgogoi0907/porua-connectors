c54a5bec811183f0eae7f091c6f9159a008ca54b
anupamgogoi@gmail.com <anupamgogoi@gmail.com>
2018-06-18 15:54:03 -0300

    porua-jms deleted

--- porua-jms/src/main/java/com/porua/jms/generated/spring/NamespaceHandler.java
+++
@@ -1,9 +0,0 @@
-package com.porua.jms.generated.spring;
-
-import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
-
-public class NamespaceHandler extends NamespaceHandlerSupport {
-  public void init() {
-    registerBeanDefinitionParser("listener",new SimpleJmsServerDefinitionParser());;
-  }
-}
