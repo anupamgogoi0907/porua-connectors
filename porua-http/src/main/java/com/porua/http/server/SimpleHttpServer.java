fd200094cb8055fb9334425a6b3b3ff330770378
anupamgogoi@gmail.com <anupamgogoi@gmail.com>
2018-06-21 18:10:23 -0300

    Log4j 2 support added

--- porua-http/src/main/java/com/porua/http/server/SimpleHttpServer.java
+++ porua-http/src/main/java/com/porua/http/server/SimpleHttpServer.java
@@ -1,6 +1,7 @@
 package com.porua.http.server;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
 import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
@@ -25,8 +26,8 @@ public class SimpleHttpServer extends MessageListener {
 	private SimpleHttpServerConfiguration config;
 
 	private HttpServer server;
-	
-	private Logger logger=Logger.getLogger(SimpleHttpServer.class);
+
+	private Logger logger = LogManager.getLogger(SimpleHttpServer.class);
 
 	/**
 	 * Starts the Http Listener.
