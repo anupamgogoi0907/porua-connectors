5c827732f3c37cbe42d8c2a17b525c65a22b5be8
anupamgogoi@gmail.com <anupamgogoi@gmail.com>
2018-06-22 12:39:05 -0300

    bug fixes

--- porua-components/src/main/java/com/porua/component/generated/palette/PoruaLoggerPalette.java
+++ porua-components/src/main/java/com/porua/component/generated/palette/PoruaLoggerPalette.java
@@ -6,7 +6,7 @@ import com.porua.core.tag.Connector;
     tagName = "porua-logger",
     tagNamespace = "http://www.porua.org/components",
     tagSchemaLocation = "http://www.porua.org/components/components.xsd",
-    imageName = "core-json-to-java.png"
+    imageName = "core-porua-logger.png"
 )
 public class PoruaLoggerPalette {
 }
--- porua-components/src/main/java/com/porua/component/generated/spring/NamespaceHandler.java
+++ porua-components/src/main/java/com/porua/component/generated/spring/NamespaceHandler.java
@@ -3,7 +3,13 @@ package com.porua.component.generated.spring;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 
 public class NamespaceHandler extends NamespaceHandlerSupport {
-  public void init() {
-    registerBeanDefinitionParser("file-connector",new PoruaFileConnectorDefinitionParser());registerBeanDefinitionParser("java-component",new JavaComponentDefinitionParser());registerBeanDefinitionParser("json-to-java",new JsonToJavaDefinitionParser());registerBeanDefinitionParser("json-to-xml",new JsonToXmlDefinitionParser());registerBeanDefinitionParser("set-payload",new PayloadSetterDefinitionParser());registerBeanDefinitionParser("set-variable",new VariableSetterDefinitionParser());;
-  }
+	public void init() {
+		registerBeanDefinitionParser("file-connector", new PoruaFileConnectorDefinitionParser());
+		registerBeanDefinitionParser("java-component", new JavaComponentDefinitionParser());
+		registerBeanDefinitionParser("json-to-java", new JsonToJavaDefinitionParser());
+		registerBeanDefinitionParser("json-to-xml", new JsonToXmlDefinitionParser());
+		registerBeanDefinitionParser("set-payload", new PayloadSetterDefinitionParser());
+		registerBeanDefinitionParser("set-variable", new VariableSetterDefinitionParser());
+		registerBeanDefinitionParser("porua-logger", new PoruaLoggerDefinitionParser());
+	}
 }
