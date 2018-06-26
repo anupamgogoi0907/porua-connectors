001fa1da21527d8fdee6898787508a5f5665961d
anupamgogoi@gmail.com <anupamgogoi@gmail.com>
2018-06-26 09:59:25 -0300

    Test case added

--- porua-http/src/test/java/com/porua/http/test/TestHttpComponent.java
+++ porua-http/src/test/java/com/porua/http/test/TestHttpComponent.java
@@ -2,17 +2,11 @@ package com.porua.http.test;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.server.Response;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -20,69 +14,30 @@ import com.porua.http.request.SimpleHttpRequester;
 import com.porua.http.server.SimpleHttpServer;
 
 public class TestHttpComponent {
+	static ClassPathXmlApplicationContext ctx = null;
+
+	@BeforeClass
+	public static void loadContext() throws Exception {
+		ctx = new ClassPathXmlApplicationContext("http-test.xml");
+	}
 
 	@Test
-	public void testHttp() throws IOException {
-
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("http-test.xml");
+	public void testHttpServer() throws IOException {
 		SimpleHttpServer server = ctx.getBean(SimpleHttpServer.class);
 		Assert.assertNotNull(server.getConfig());
+
+	}
+
+	@Test
+	public void testHttpRequester() throws Exception {
 		SimpleHttpRequester requester = ctx.getBean(SimpleHttpRequester.class);
 		assertNotNull(requester);
-		// requester.process();
-		// System.in.read();
-		ctx.close();
 	}
 
-	// @Test
-	public void testServer() {
-		Map<String, Object> mapHeader = new HashMap<>();
-		try {
-			HttpServer server = HttpServer.createSimpleServer(null, "localhost", 8080);
-			server.getServerConfiguration().addHttpHandler(new HttpHandler() {
-
-				@Override
-				public void service(Request req, Response res) throws Exception {
-					InputStream is = req.getInputStream();
-					convertToString(is);
-					// Extract Headers.
-					Iterable<String> headers = req.getHeaderNames();
-					for (String h : headers) {
-						mapHeader.put(h, req.getHeader(h));
-					}
-
-					// Extract Parameters.
-					Map<String, String[]> mapParam = req.getParameterMap();
-					for (String p : mapParam.keySet()) {
-						String[] params = mapParam.get(p);
-						System.out.println(params.length);
-					}
-
-				}
-			}, "/hello");
-			server.start();
-			System.out.println("started...");
-			System.in.read();
-		} catch (IOException e) {
-			e.printStackTrace();
+	@AfterClass
+	public static void closeContext() throws Exception {
+		if (ctx != null) {
+			ctx.close();
 		}
-	}
-
-	public String convertToString(InputStream inputStream) {
-		String res = "";
-		try {
-			ByteArrayOutputStream result = new ByteArrayOutputStream();
-			byte[] buffer = new byte[1024];
-			int length;
-			while ((length = inputStream.read(buffer)) != -1) {
-				result.write(buffer, 0, length);
-			}
-			res = result.toString("UTF-8");
-			System.out.println(res);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		return res;
-
 	}
 }
