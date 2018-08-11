e1f0d25bf94a33f64ef60da538c80ed097df471f
Anupam Gogoi <anupamgogoi@gmail.com>
2018-08-11 20:33:50 -0300

    code gen modified

--- porua-api-router/src/main/java/com/porua/api/utility/ApiGen.java
+++ porua-api-router/src/main/java/com/porua/api/utility/ApiGen.java
@@ -14,9 +14,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.UriInfo;
 
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
@@ -30,6 +34,10 @@ import io.swagger.parser.SwaggerParser;
 public class ApiGen {
 
 	private static String SRC_FILE = "src/main/java";
+
+	public static void main(String[] args) throws Exception {
+		generateApiClass("api.yaml");
+	}
 
 	public static void generateApiClass(String apiPath) throws Exception {
 		List<MethodSpec> listMethod = new ArrayList<>();
@@ -55,14 +63,21 @@ public class ApiGen {
 			}
 		});
 
-		// Root class annotations.
+		// API class annotations.
 		List<AnnotationSpec> listClassAnnot = new ArrayList<>();
 		listClassAnnot.add(AnnotationSpec.builder(Path.class)
 				.addMember("value", CodeBlock.of("$S", api.getBasePath() == null ? "/" : api.getBasePath())).build());
 		listClassAnnot.add(AnnotationSpec.builder(Api.class).addMember("value", CodeBlock.of("$S", "Test")).build());
 
+		// API class fields.
+		List<FieldSpec> listField = new ArrayList<>();
+		listField.add(FieldSpec.builder(UriInfo.class, "uriInfo")
+				.addAnnotation(AnnotationSpec.builder(Context.class).build()).build());
+		listField.add(FieldSpec.builder(HttpHeaders.class, "headers")
+				.addAnnotation(AnnotationSpec.builder(Context.class).build()).build());
+
 		TypeSpec className = TypeSpec.classBuilder("MyAPI").addModifiers(Modifier.PUBLIC).addAnnotations(listClassAnnot)
-				.addMethods(listMethod).build();
+				.addFields(listField).addMethods(listMethod).build();
 		JavaFile javaFile = JavaFile.builder("anupam.generated", className).build();
 		javaFile.writeTo(new File(SRC_FILE));
 	}
